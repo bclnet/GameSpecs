4a0bf441ca77448b9f144bf496a542848d4c291a
Sky Morey <apphaus@outlook.comgit config --global user.name Sky Morey>
2023-02-05 08:29:09 -0600

    updates

--- Base/GameSpec.Base/FileManagers/MacOsFileManager.cs
+++ Base/GameSpec.Base/FileManagers/MacOsFileManager.cs
@@ -13,9 +13,31 @@ namespace GameSpec.FileManagers
         {
             base.ParseFileManager(elem);
             if (!elem.TryGetProperty("macos", out var z)) return this;
-            //elem = z;
+            elem = z;
+
+            AddRegistry(elem);
+            AddDirect(elem);
+            AddIgnores(elem);
+            AddFilters(elem);
 
             return this;
+        }
+
+        protected void AddRegistry(JsonElement elem)
+        {
+            if (!elem.TryGetProperty("registry", out var z)) return;
+            foreach (var prop in z.EnumerateObject())
+                if (prop.Value.TryGetProperty("key", out z))
+                {
+                    var keys = z.ValueKind switch
+                    {
+                        JsonValueKind.String => new[] { z.GetString() },
+                        JsonValueKind.Array => z.EnumerateArray().Select(y => y.GetString()),
+                        _ => throw new ArgumentOutOfRangeException(),
+                    };
+                    foreach (var key in keys)
+                        if (TryGetRegistryByKey(key, prop, prop.Value.TryGetProperty(key, out z) ? (JsonElement?)z : null, out var path)) AddPath(prop, path);
+                }
         }
 
         #endregion
